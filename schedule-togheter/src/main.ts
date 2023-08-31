3aae4a0795b46ed5608ab26482d1767bd3265e59
MarcoVitchiThulin <142972250+MarcoVitchiThulin@users.noreply.github.com>
2023-08-31 14:30:45 +0200

    Fix file structure

--- schedule-togheter/src/main.ts
+++ schedule-togheter/src/main.ts
@@ -1,14 +1,16 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from './App.vue';
+import { router } from './router';
 
-import App from './App.vue'
-import router from './router'
+// setup fake backend
+import { fakeBackend } from './helpers';
+fakeBackend();
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
