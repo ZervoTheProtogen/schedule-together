f53130a5031b79cc93c003a0456196eab91e0d03
ZervoTheProtogen <marco@zervo.org>
2023-09-21 12:12:26 +0200

    Attempt to create schoolsoft iframe thing

--- frontend/src/helpers/get-status.js
+++ frontend/src/helpers/get-status.js
@@ -1,13 +1,16 @@
-
 // TODO: Rewrite to TypeScript
 
-export default function getStatus(url) {
-    var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if (request.readyState === 4){
-        request.status;//this contains the status code
+export async function getStatus(url) {
+    try {
+        const response = await fetch(url);
+        const isSuccessful = response.ok;
+    
+        if (isSuccessful) {
+            if (!isNaN(response.status)) {
+                return "error"
+            } else return response.status
+        }
+    } catch (error) {
+        console.log(error)
     }
-};
-request.open("GET", url , true);
-request.send(null);
 }
--- frontend/src/views/schedule/add/SchoolSoft.vue
+++ frontend/src/views/schedule/add/SchoolSoft.vue
@@ -2,15 +2,16 @@
 import { storeToRefs } from 'pinia';
 import { useAuthStore } from '@/stores';
 import { router } from '@/router';
-import { getStatus } from '@/helpers'
+import { getStatus } from '@/helpers';
 import { ref } from 'vue';
+
+import { Spinner } from '@/components';
 
 const authStore = useAuthStore();
 const { user } = storeToRefs(authStore);
 
-const schoolCode = ref('');
-const returnCode = ref(0);
-
+var desUrl = ref('');
+var returnCode = ref(1);
 </script>
 
 <template>
@@ -18,14 +19,22 @@ const returnCode = ref(0);
     <h1 style="text-align: center;">Add Schedule</h1>
     <div class="content-spacer-small"></div>
     <div class="card-container">
-        <div v-if="schoolCode == ''" style="text-align: center;">
+        <div v-if="desUrl == ''" style="text-align: center;">
             <p>Enter school code</p>
             <div class="card-form-group" style="margin-bottom:10px;">
-                <input name="searchbox" type="text" placeholder="input code.." v-model="schoolCodeLive" class="card-form-control" style="height:35px;"/>
+                <input name="searchbox" type="text" placeholder="input code.." v-model="schoolCode" class="card-form-control" style="height:35px;"/>
             </div>
             <div class="card-form-row">
                 <button style="margin-right:20px;" class="card-form-button negative" @click="router.push('/schedule/add')">Cancel</button>
-                <button class="card-form-button" @click="schoolCode = schoolCodeLive">Continue</button>
+                <button class="card-form-button" @click="desUrl = `https://sms.schoolsoft.se/${schoolCode}/html/redirect_login.htm`">Continue</button>
+            </div>
+        </div>
+        <div v-else>
+            <div v-if="returnCode == 0">
+                <Spinner />
+            </div>
+            <div v-else class="card">
+                <iframe :src="desUrl" title="SchoolSoft" width="500px" height="700px" sandbox="allow-forms allow-scripts allow-same-origin allow-popups">aaa</iframe>
             </div>
         </div>
         <div></div>
